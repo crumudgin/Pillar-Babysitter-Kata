fe743c3c5d8329a2ed91464612477d79eda1a2f7
Zac Chu <zac.chu@gmail.com>
2019-02-12 14:18:12 -0500

    REFACTOR: refactored family class to use single line conditional statment.

--- family.py
+++ family.py
@@ -6,8 +6,6 @@ class Family():
 		self.name = name
 		temp_lst = []
 		for price, hour in price_by_hour:
-			if price == 0:
-				temp_lst.append((price, ceil(hour)))
-			else:
-				temp_lst.append((price, floor(hour)))
+			rounded_hour = ceil(hour) if price == 0 else floor(hour)
+			temp_lst.append((price, rounded_hour))
 		self.price_by_hour = tuple(temp_lst)

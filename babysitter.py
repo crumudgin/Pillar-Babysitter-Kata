c2a63845fbcd4b7d4da7f924e0816ecbbede2609
Zac Chu <zac.chu@gmail.com>
2019-02-12 11:44:01 -0500

    refactored babysitter so the constants can be moddified if necessary.

--- babysitter.py
+++ babysitter.py
@@ -1,6 +1,8 @@
 from datetime import time
 
 class Babysitter():
-	earliest_start_time = time(hour=17)
-	latest_end_time = time(hour=4)
-	max_jobs = 1
+	
+	def __init__(self, earliest_start_time = time(hour=17), latest_end_time = time(hour=4), max_jobs = 1):
+		self.earliest_start_time = earliest_start_time
+		self.latest_end_time = latest_end_time
+		self.max_jobs = max_jobs

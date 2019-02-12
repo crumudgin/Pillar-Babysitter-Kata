c013baf85604beb9361f0a44e0462eea4017afd7
Zac Chu <zac.chu@gmail.com>
2019-02-12 15:39:50 -0500

    REFACTOR: Seperated the loop that rounds the hour out into it's own function.

--- family.py
+++ family.py
@@ -4,8 +4,11 @@ class Family():
 
 	def __init__(self, name, price_by_hour):
 		self.name = name
-		temp_lst = []
-		for price, hour in price_by_hour:
+		self.price_by_hour = tuple(self.round_hour(price_by_hour))
+
+	def round_hour(self, price_and_hours):
+		rounded_price_by_hour = []
+		for price, hour in price_and_hours:
 			rounded_hour = ceil(hour) if price == 0 else floor(hour)
-			temp_lst.append((price, rounded_hour))
-		self.price_by_hour = tuple(temp_lst)
+			rounded_price_by_hour.append((price, rounded_hour))
+		return rounded_price_by_hour

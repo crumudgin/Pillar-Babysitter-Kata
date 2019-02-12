aa6280eb8956dafa81e11505fe4164c09711fa47
Zac Chu <zac.chu@gmail.com>
2019-02-12 11:58:45 -0500

    REFACTOR: refactored job class so that earliest_start_time and latest_end_time can be adjusted if neccessary.

--- job.py
+++ job.py
@@ -1,8 +1,8 @@
 from datetime import time
 
 class Job():
-	earliest_start_time = time(hour=17)
-	latest_end_time = time(hour=4)
 
-	def __init__(self, hours):
-		pass
+	def __init__(self, hours, earliest_start_time = time(hour=17), latest_end_time = time(hour=4)):
+		self.earliest_start_time = earliest_start_time
+		self.latest_end_time = latest_end_time
+		self.hours = hours

7e18d0c70c20c4981e9aaaa56470f8ce18ca49cc
Zac Chu <zac.chu@gmail.com>
2019-02-12 17:43:20 -0500

    GREEN: Added code for take_job in babysitter.

--- babysitter.py
+++ babysitter.py
@@ -6,3 +6,13 @@ class Babysitter():
 		self.earliest_start_time = earliest_start_time
 		self.latest_end_time = latest_end_time
 		self.max_jobs = max_jobs
+		self.jobs = []
+
+	def take_job(self, job):
+		if len(self.jobs) >= self.max_jobs:
+			raise ValueError("The babysitter is only axxepting %d job(s) per night" %self.max_jobs)
+		for start, stop in job.hours:
+			if min(start, stop) < self.earliest_start_time or max(start, stop) > self.latest_end_time:
+				raise ValueError("The babysitter starts no earlier than %d and ends no later than %d" %(self.earliest_start_time, self.latest_end_time))
+		self.jobs.append(job)
+			

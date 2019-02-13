5db73d5ea629f727ec8daf7e88e04b674c05949b
Zac Chu <zac.chu@gmail.com>
2019-02-13 10:46:44 -0500

    RED: updated all tests to use datetime

--- Tests/babysitter_test.py
+++ Tests/babysitter_test.py
@@ -40,17 +40,17 @@ def convert_price_hour_tuples(tup):
 A test to ensure that the constants of the babysitter match the requirements.
 """
 def test_babysitter_constants(babysitter):
-	assert babysitter.earliest_start_time == 0
-	assert babysitter.latest_end_time == 10
+	assert babysitter.earliest_start_time == datetime(2019, 2, 13, 17)
+	assert babysitter.latest_end_time == datetime(2019, 2, 14, 4)
 	assert babysitter.max_jobs == 1
 
 @pytest.mark.parametrize(("hours"),
-						[((0, 0), (0, 10)),
-						 ((0, 1), (0, 10)),
+						[((0, 0), (0, 11)),
+						 ((0, 1), (0, 11)),
 						 ((0, 1), (0, 8)),
-						 ((0, 5), (0, 10)),
-						 ((0, 0), (0, 5), (0, 10)),
-						 ((0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10))
+						 ((0, 5), (0, 11)),
+						 ((0, 0), (0, 5), (0, 11)),
+						 ((0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10), (0, 11))
 						])
 def test_babysitter_take_valid_job(babysitter, hours):
 	job = mock.Mock()
@@ -59,29 +59,29 @@ def test_babysitter_take_valid_job(babysitter, hours):
 	assert babysitter.jobs == [job]
 
 
-# @pytest.mark.parametrize(("hours"),
-# 						[((0, 0), (0, 11)),
-# 						 ((0, 11), (0, 12)),
-# 						 ((0, -1), (0, 1))
-# 						])
-# def test_babysitter_take_invalid_job(babysitter, hours):
-# 	job = mock.Mock()
-# 	job.configure_mock(hours=hours)
-# 	with pytest.raises(ValueError) as excinfo:
-# 		babysitter.take_job(job)
-# 	error_msg = "The babysitter starts no earlier than %d and ends no later than %d" %(babysitter.earliest_start_time, babysitter.latest_end_time)
-# 	assert error_msg in str(excinfo)
-# 	assert babysitter.jobs == []
+@pytest.mark.parametrize(("hours"),
+						[((0, 0), (0, 12)),
+						 ((0, 13), (0, 14)),
+						 ((0, -1), (0, 1))
+						])
+def test_babysitter_take_invalid_job(babysitter, hours):
+	job = mock.Mock()
+	job.configure_mock(hours=convert_price_hour_tuples(hours))
+	with pytest.raises(ValueError) as excinfo:
+		babysitter.take_job(job)
+	error_msg = "The babysitter works from %d - %d" %(babysitter.earliest_start_time.hour, babysitter.latest_end_time.hour)
+	assert error_msg in str(excinfo)
+	assert babysitter.jobs == []
 
-# def test_babysitter_add_multiple_jobs(babysitter):
-# 	job1 = mock.Mock()
-# 	job2 = mock.Mock()
-# 	job1.configure_mock(hours=((0, 1), (0, 2)))
-# 	babysitter.take_job(job1)
-# 	with pytest.raises(ValueError) as excinfo:
-# 		babysitter.take_job(job2)
-# 	assert "The babysitter is only axxepting %d job(s) per night" %babysitter.max_jobs in str(excinfo)
-# 	assert babysitter.jobs == [job1]
+def test_babysitter_add_multiple_jobs(babysitter):
+	job1 = mock.Mock()
+	job2 = mock.Mock()
+	job1.configure_mock(hours=(convert_price_hour_tuples(((0, 1), (0, 2)))))
+	babysitter.take_job(job1)
+	with pytest.raises(ValueError) as excinfo:
+		babysitter.take_job(job2)
+	assert "The babysitter is only axxepting %d job(s) per night" %babysitter.max_jobs in str(excinfo)
+	assert babysitter.jobs == [job1]
 
 
 @pytest.mark.parametrize(("earnings"),

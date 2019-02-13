a34569638dbc53bc559de5642e5096ff064a5ed0
Zac Chu <zac.chu@gmail.com>
2019-02-12 19:14:44 -0500

    GREEN: Added family variable to Job

--- job.py
+++ job.py
@@ -1,6 +1,5 @@
 class Job():
 
-	def __init__(self, hours, earliest_start_time = 0, latest_end_time = 10):
-		self.earliest_start_time = earliest_start_time
-		self.latest_end_time = latest_end_time
+	def __init__(self, hours, family):
 		self.hours = hours
+		self.family = family

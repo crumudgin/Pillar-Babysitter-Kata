497612783571748c260963691f42a43a875af50a
Zac Chu <zac.chu@gmail.com>
2019-02-13 13:07:48 -0500

    RED: Added family creation test with invalid hours.

--- Tests/family_test.py
+++ Tests/family_test.py
@@ -14,3 +14,12 @@ def test_family_creation(name, hours, expected_hours):
 	family = Family(name, converted_hour)
 	assert family.name == name
 	assert family.price_by_hour == convert_price_hour_tuples(expected_hours)
+
+@pytest.mark.parametrize(("name",				"hours"),
+						[("hour_given_twice",	((15,0),(30,0),(0,7)))
+						])
+def test_invalid_hours(name, hours):
+	converted_hour = convert_price_hour_tuples(hours)
+	with pytest.raises(ValueError) as excinfo:
+		family = Family(name, converted_hour)
+	assert "Illegal hours" in str(excinfo)

173c09f98f20a51794d926bb1d0b79795ca35a09
Zac Chu <zac.chu@gmail.com>
2019-02-13 12:47:39 -0500

    RED: added test to make sure that the family price_by_hour is sorted

--- Tests/family_test.py
+++ Tests/family_test.py
@@ -5,8 +5,9 @@ from helper_functions import *
 
 
 @pytest.mark.parametrize(("name",			"hours",					"expected_hours"),
-						[("whole_number",	((15,0),(30,5),(0,7)),		((15,0), (30,5),(0,7))),
-						 ("decimal_number",	((15,0.3),(40,5.59),(0,7.1)),((15,0), (40,5),(0,8)))
+						[("whole_number",	((15,0),(30,5),(0,7)),			((15,0), (30,5),(0,7))),
+						 ("decimal_number",	((15,0.3),(40,5.59),(0,7.1)),	((15,0), (40,5),(0,8))),
+						 ("unsorted",		((15,10),(20,0)),				((20,0), (15,10)))
 						])
 def test_family_creation(name, hours, expected_hours):
 	converted_hour = convert_price_hour_tuples(hours)

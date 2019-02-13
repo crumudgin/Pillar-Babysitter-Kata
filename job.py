419903ff00dc37af1976278f31d983571bffd76b
Zac Chu <zac.chu@gmail.com>
2019-02-13 10:17:29 -0500

    REFACTOR: Moved the error checking out into its own function.

--- job.py
+++ job.py
@@ -1,7 +1,10 @@
 class Job():
 
 	def __init__(self, hours, family):
-		if hours[1] <= hours[0]:
-			raise ValueError("Illegal job hours")
+		self.check_hours_validity(hours[0], hours[1])
 		self.hours = hours
 		self.family = family
+
+	def check_hours_validity(self, start, stop):
+		if stop <= start:
+			raise ValueError("Illegal job hours")

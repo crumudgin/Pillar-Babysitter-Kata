6e650b824b4145a6b7b157ff799520ca7cc70b25
Zac Chu <zac.chu@gmail.com>
2019-02-12 19:13:55 -0500

    RED: added tests for job creation

--- Tests/job_test.py
+++ Tests/job_test.py
@@ -6,10 +6,19 @@ from job import Job
 """
 A test to ensure that the constants of the job match the requirements.
 """
-def test_job_creation():
-	hours = []
+@pytest.mark.parametrize(("hour"),
+						[
+						(0, 2),
+						(0, 10),
+						(2, 10),
+						(5, 6),
+						(20, 23)
+						])
+def test_valid_job_creation(hour):
+	hours = hour
 	family = mock.Mock()
 	job = Job(hours, family)
 	assert hours == job.hours
 	assert family == job.family
+
 

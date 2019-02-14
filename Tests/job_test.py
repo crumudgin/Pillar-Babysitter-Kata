d343b7f39393ed9c050e4c4ec2eb4daff71f1ea6
Zac Chu <zac.chu@gmail.com>
2019-02-14 09:00:04 -0500

    RED: Added a bunch of tests for calculate_pay.

--- Tests/job_test.py
+++ Tests/job_test.py
@@ -34,9 +34,14 @@ def test_invalid_job_creation(family, hours):
 		job = Job(hours, family)
 	assert "Illegal job hours" in str(excinfo)
 
-@pytest.mark.parametrize(("hours", "pay", 		"expected_output"),
-						[((0, 10),	((1, 0),),	10),	#test that the function calculates the hours for a single pay range
-						 ((0, 10),	((2, 0),),	20)		#test that the function works for multiple inputs					
+@pytest.mark.parametrize(("hours", "pay", 								"expected_output"),
+						[((0, 10),	((1, 0),),							10),#test that the function calculates the hours for a single pay range
+						 ((0, 10),	((2, 0),),							20),#test that the function works for multiple inputs
+						 ((0, 10),	((1, 0), (2, 5)),					15),#test that the function works with multiple time ranges
+						 ((2, 8),	((2, 0), (3, 5)),					15),#test that the function works for any legal hour range
+						 ((0, 10),	((2, 2), (4, 5)),					26),#test the outcome when the pay starts later than the hours
+						 ((0, 10),	((5, 0), (0, 5)),					25),#test the outcome when the pay stops earlier than the hours
+						 ((0, 4),	((1, 0), (2, 1), (3, 2), (4, 3)),	10)	#test the output with many different pay ranges
 						])
 def test_calculate_pay(hours, pay, expected_output):
 	family = mock.Mock()
